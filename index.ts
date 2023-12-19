b9f064c0f18cbc8f87a724bfad7c06bbe90e895c
Jihed Bedhief <jihedbndief22@gmail.com>
2023-12-19 05:43:55 +0100

    adding routes

--- index.ts
+++ index.ts
@@ -1,6 +1,19 @@
-const z =10 
-const read = (text:string )=>{
-    return text
-}
-const msg = read("hello word !@!")
-console.log(msg);
+import figlet from "figlet";
+
+const server = Bun.serve({
+    port :3000,
+    fetch(request) {
+        const url = new URL(request.url);
+        if(url.pathname===("/")){
+            const body = figlet.textSync("ahla byk si bun ")
+            return new Response(body)
+        }
+        if(url.pathname===("/about")){
+            
+            return new Response("about me ")
+        }
+        
+        return new Response("404 !")
+    },
+})
+console.log(`Listening on http://localhost:${server.port} ...`);
